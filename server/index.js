c69a592f85a7ffeed9f47345580a2e72a1a79a9a
Ahmet Talha Keles <ahmettalhakeles@users.noreply.github.com>
2026-07-02 14:28:20 +0300

    fix(deploy): add /health endpoint for Railway healthcheck

--- server/index.js
+++ server/index.js
@@ -39,6 +39,10 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
